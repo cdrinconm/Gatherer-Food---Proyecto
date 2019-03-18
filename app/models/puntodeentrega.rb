06944cd7d8cd24e5ab8c88e0f81fb91ebcb08702
cdrinconm <cdrinconm@unal.edu.co>
2019-03-18 03:14:05 +0000

    .

--- app/models/puntodeentrega.rb
+++ app/models/puntodeentrega.rb
@@ -1,5 +1,5 @@
 class Puntodeentrega < ApplicationRecord
   validates :nombre, presence: true, length: {minimum: 1, maximum: 50}
-  validates :habitantesBeneficiados, presence: true, length: {minimum: 0}
+  validates :habitantesBeneficiados, presence: true
   validates :ubicacion, presence: true, length: {minimum: 1, maximum: 500}
 end
